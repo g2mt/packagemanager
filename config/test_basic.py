98b33e0af421f8ab8385ed6fc4a7858165e3c845
g2mt <pm2mtr@gmail.com>
2026-05-01 05:03:37 +0000

    feat: add basic1 package and set version for basic

--- config/test_basic.py
+++ config/test_basic.py
@@ -1,6 +1,14 @@
 @m.package(
     name="basic",
+    version=lambda pkg: "1.0",
     tags=["tag1", "tag2"]
 )
 def basic():
+    print("Hello World")
+
+@m.package(
+    name="basic1",
+    version="1.0",
+)
+def basic1():
     print("Hello World")
